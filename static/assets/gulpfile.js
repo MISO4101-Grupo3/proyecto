72cdf431eb5ef3553b857ba7697e1180f5367f1c
Juan Sebastian Hernandez Serrato <hernanjua@colpatria.com>
2018-04-16 20:52:31 -0500

    modified css generation with minification task

--- static/assets/gulpfile.js
+++ static/assets/gulpfile.js
@@ -3,10 +3,12 @@ var sass = require('gulp-sass');//sass complie
 var concat = require('gulp-concat'); //compile js plugins into one file
 var concatCss = require('gulp-concat-css');//compile css plugins into one file
 var watch = require('gulp-watch'); //sass compile to css
+var minifyCSS = require('gulp-minify-css'); //minify compiled css
 
 gulp.task('sass', function () {
     return gulp.src('scss/**/*.scss')
         .pipe(sass().on('error', sass.logError))
+        .pipe(minifyCSS())
         .pipe(gulp.dest('css/'));
 });
 gulp.task('watch', function () {
