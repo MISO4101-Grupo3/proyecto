6bf0e03f1a0b8716be735129f04fb8c388aa5c36
leonardo merchan <leonardo.merchan.1992@gmail.com>
2018-04-22 16:17:09 -0500

    Test Base Persona_De_Conectate

--- webapp/tests.py
+++ webapp/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from webapp.models import Persona_De_Conectate
 
-# Create your tests here.
+class Persona_De_ConectaTestCase(TestCase):
+    """
+    PersonTestCase has all tests of the Person Model for the app.
+    """
+    def setUp(self):
+        # """
+        #     SetUp de Test Data
+        # """
+        # Person.objects.create(name="Tester", last_name="1")
+        Persona_De_Conectate.objects.create(nombre="Leonardo", perfil="Desarrollador Web")
+
+    def test_nombre(self):
+        """
+        Test: Test Full Name
+        Check the full name of a person
+        """
+        tester1 = Persona_De_Conectate.objects.get(id=1)
+        self.assertEqual(tester1.nombre, 'Leonardo')
+
+    def test_perfil(self):
+        """
+        Test: Test Full Name
+        Check the full name of a person
+        """
+        tester1 = Persona_De_Conectate.objects.get(id=1)
+        self.assertEqual(tester1.perfil, 'Desarrollador Web')
